fed7c515ecf78428a347fca531386ac77d926fea
Nezar Abdennur <nabdennur@gmail.com>
2026-02-17 10:28:09 -0500

    ci: Configure gh-pages build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import { resolve } from "node:path";
+import { readdirSync } from "node:fs";
 import { defineConfig } from "vite";
 
+const root = resolve(__dirname, "examples");
+const htmlFiles = readdirSync(root).filter((f) => f.endsWith(".html"));
+
+const input: Record<string, string> = {};
+for (const file of htmlFiles) {
+	input[file.replace(".html", "")] = resolve(root, file);
+}
+
 export default defineConfig({
+	root: "examples",
+	publicDir: false,
 	resolve: {
 		alias: {
 			"@grandscatter/core": resolve(__dirname, "src/index.ts"),
 		},
 	},
-	server: {
-		open: "/examples/",
+	build: {
+		target: "esnext",
+		outDir: resolve(__dirname, "dist"),
+		emptyOutDir: true,
+		rollupOptions: {
+			input,
+		},
 	},
 });
